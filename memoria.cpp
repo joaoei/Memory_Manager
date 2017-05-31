6e5caedbe4513ddc3761178e8764c14b55f94584
João Emmanuel <joaoeizidio@gmail.com>
2017-05-30 22:50:24 -0300

    % da Memória, Swap e falta de paginas para cada processo

--- memoria.cpp
+++ memoria.cpp
@@ -7,59 +7,94 @@
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
+#include <cstring>
+# include <dirent.h>
 using namespace std;
 
 /*Variáveis*/
-FILE *memInfo;
-FILE *swaps;
-char s[7], kb[2];
-char s1[9], s2[4], s3[4], s4[4], s5[9];
-int n,s_total, s_used, s_priority;
+char caminho[256],cmd[256],s1[9], s2[9], s3[9], s4[9];
 
-void rotina_memInfo(){
-
-	memInfo = fopen("/proc/meminfo", "r");
-	if(memInfo == NULL){
-		printf("Erro, nao foi possivel abrir o arquivo\n");
-	}else{
-		fscanf(memInfo,"%s %d %s\n",s,&n,kb);
-		int total = n;
-		fscanf(memInfo,"%s %d %s\n",s,&n,kb);
-		int livre = n;
-		int mem_usada = (100 * (total - livre)) / total;
-		cout << "Memória total: " << total << " kB \n";
-		cout << "Memoria usada: " << mem_usada << "% \n";
-		fscanf(memInfo,"%s %d %s\n",s,&n,kb);
-		fscanf(memInfo,"%s %d %s\n",s,&n,kb);
-		fscanf(memInfo,"%s %d %s\n",s,&n,kb);
-		int cache_usada = n;
-		cout << "Cache usada:   " << cache_usada << " kB \n";
-	}
-	fclose(memInfo);	
-}
-void rotina_swaps(){
-   	
-	swaps = fopen("/proc/swaps", "r");
+void swap(string pid, int *p_swap) {
+	FILE *swaps;
+	strcpy(cmd, "");
+	strcat(cmd, "grep VmSwap /proc/");
+	strcat(cmd, pid.c_str());
+	strcat(cmd, "/status > swap.txt");
+	system(cmd);
+	swaps = fopen("swap.txt", "r");
 	if(swaps == NULL){
 		printf("Erro, nao foi possivel abrir o arquivo\n");
 	}else{
-		fscanf(swaps,"%s %s %s %s %s\n",s1,s2,s3,s4,s5);
-		fscanf(swaps,"%s %s %d %d %d\n",s1,s5, &s_total, &s_used, &s_priority);
-		int swap_usado = (100 * s_used) / s_total;
-		cout << "Swap total:    " <<  s_total << " kB \n";
-		cout << "Swap usado:    " <<  swap_usado << "% \n";
+		fscanf(swaps,"%s %d %s\n",s1,p_swap,s3);
 	}
-	fclose(swaps);	
+	fclose(swaps);
 }
-int main(int argc, char *argv[]){
-	
-	while(true){
-		system("ps -eo pid,min_flt,maj_flt,comm | awk '$2!=0'");
-		rotina_memInfo();
-		rotina_swaps();
-		sleep(2);
-		system("clear");
+
+void mem_info(string pid, float *mem, int *minfl, int *majfl) {
+	FILE *memInfo;
+	strcpy(cmd, "");
+	strcat(cmd, "ps -o pmem,min_flt,maj_flt ");
+	strcat(cmd, pid.c_str());
+	strcat(cmd, " > mem_infos.txt");
+	system(cmd);
+	memInfo = fopen("mem_infos.txt", "r");
+	if(memInfo == NULL){
+		printf("Erro, nao foi possivel abrir o arquivo\n");
+	}else{
+		fscanf(memInfo,"%s %s %s\n",s2,s3,s4);
+		fscanf(memInfo,"%f %d %d\n",mem,minfl,majfl);
 	}
-	
+	fclose(memInfo);
+}
+
+int main(int argc, char *argv[]){
+	int v = 1; 
+	int p_swap;
+	float mem;
+	char caminho[256];
+	int minfl, majfl;
+	string pid = "11401";
+	DIR *dirp;
+
+	while(1) {
+		system("clear");
+		struct dirent *directory_entry;
+
+		if ((dirp = opendir("/proc")) == NULL) {
+			perror("Nao foi possivel abri /proc");
+			return 1;
+		}
+
+		do {
+			if ((directory_entry = readdir(dirp)) != NULL) {
+				if (directory_entry->d_type == DT_DIR) {
+					string nome = directory_entry->d_name;
+					int b = nome.size();
+					int d=0;
+					for(int c=0;c<b;c++) {
+				        if(isdigit(directory_entry->d_name[c])){
+				       		d++;
+				        }
+					}
+					if (d==b) {
+						strcpy(caminho, "");
+						strcat(caminho, "/proc/");
+						strcat(caminho, directory_entry->d_name);
+								
+						pid = nome;
+						swap(pid, &p_swap);			
+						//cache(pid,&p_cache);
+						mem_info(pid, &mem, &minfl, &majfl);
+						cout << "PID: " << pid << " Cache: " << "kkkkk" << "  Swap: " << p_swap << " kB  PageFaults Minor: " << minfl << "  PageFaults Major: " << majfl << "\n"; 
+					}
+				}
+			}
+		} while (directory_entry != NULL);
+		sleep(1);
+	}
+
+	//ESCREVRE TUDO EM UM ARQUIVO E DPS MOSTRA-LO
+	//DETERMINAR O VALOR DA CACHE
+	//ORGANIZAR VISUALMENTE
 return 0;
 }
